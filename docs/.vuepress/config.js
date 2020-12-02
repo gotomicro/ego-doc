7d57bdf78558515b821682388aa18f120a0e82a4
askuy <askuygo@foxmail.com>
2020-12-02 21:42:05 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,72 @@
+const moment = require("moment");
+module.exports = {
+    title: "EGO",
+    description: "最简单的GO框架",
+    head: [
+        ["link", { rel: "icon", href: "/logo.png" }],
+        [
+            "meta",
+            {
+                name: "keywords",
+                content: "Go,golang,ego,micro service,gRPC",
+            },
+        ],
+    ],
+
+    markdown: {
+        lineNumbers: true, // 代码块显示行号
+    },
+    themeConfig: {
+        nav: [
+            {
+                text: "首页",
+                link: "/",
+            },
+            {
+                text: "框架",
+                link: "/frame/",
+            },
+            {
+                text: "EGO",
+                link: "https://github.com/gotomicro/ego",
+            },
+        ],
+        docsDir: "docs",
+        docsBranch: "main",
+        editLinks: true,
+        editLinkText: "在github.com上编辑此页",
+        sidebar: {
+            "/summary/": [""], //这样自动生成对应文章
+            "/frame/": [], //这样自动生成对应文章
+        },
+        sidebarDepth: 2,
+        lastUpdated: "上次更新",
+        serviceWorker: {
+            updatePopup: {
+                message: "发现新内容可用",
+                buttonText: "刷新",
+            },
+        },
+    },
+    plugins: [
+        [
+            "@vuepress/last-updated",
+            {
+                transformer: (timestamp, lang) => {
+                    // 不要忘了安装 moment
+                    const moment = require("moment");
+                    moment.locale("zh-cn");
+                    return moment(timestamp).format("YYYY-MM-DD HH:mm:ss");
+                },
+
+                dateOptions: {
+                    hours12: true,
+                },
+            },
+        ],
+        "@vuepress/back-to-top",
+        "@vuepress/active-header-links",
+        "@vuepress/medium-zoom",
+        "@vuepress/nprogress",
+    ],
+};
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroImage: /logo.png
+actionText: 快速上手 →
+actionLink: /frame/
+heroText: EGO
+tagline: 简单的Go框架
+sidebar: auto
+features:
+- title: 大道至简
+  details: 组件行为统一
+- title: 自动治理
+  details: 组件拦截器
+- title: 工程哲学
+  details: 配置驱动组件，易于使用
+
+footer: MIT Licensed | Copyright © 2020-present EGO
+---
