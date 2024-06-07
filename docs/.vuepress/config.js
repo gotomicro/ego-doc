892ed77aa5ae206754645cd74efc756f101a12c6
askuy <askuygo@foxmail.com>
2024-06-07 11:56:09 +0800

    dashboard

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -188,6 +188,7 @@ var _hmt = _hmt || [];
                     children: [
                         "sla/overview",
                         "sla/monitor",
+                        "sla/dashboard",
                     ],
                 },
             ],
